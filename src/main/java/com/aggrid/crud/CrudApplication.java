01705d7257bcc6479e0688b965d40838424a6b9b
Sean Landsman <sean@thelandsmans.com>
2017-09-13 10:49:12 +0100

    First commit - initialzr boilerplate added

---
+++ src/main/java/com/aggrid/crud/CrudApplication.java
@@ -0,0 +1,12 @@
+package com.aggrid.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+}
