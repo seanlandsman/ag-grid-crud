8ae05e1e7e5a3afcc631bda2e891823838206f76
Sean Landsman <sean@thelandsmans.com>
2017-09-22 16:20:09 +0100

    Backend up to initial rest service

---
+++ src/main/java/com/aggrid/crudapp/CrudAppApplication.java
@@ -0,0 +1,12 @@
+package com.aggrid.crudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudAppApplication.class, args);
+    }
+}
