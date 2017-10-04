a415f82c83f342a0dd83b059b7c41524eb55b78b
Sean Landsman <sean@thelandsmans.com>
2017-10-04 15:45:00 +0100

    Add controller and controller test

--- src/main/java/com/aggrid/crudapp/controllers/AthleteController.java
+++ src/main/java/com/aggrid/crudapp/controllers/AthleteController.java
@@ -1,46 +1,22 @@
 package com.aggrid.crudapp.controllers;
 
 import com.aggrid.crudapp.model.Athlete;
-import com.aggrid.crudapp.model.Result;
-import com.aggrid.crudapp.model.Sport;
 import com.aggrid.crudapp.repositories.AthleteRepository;
-import com.aggrid.crudapp.repositories.SportRepository;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class AthleteController {
     private AthleteRepository athleteRepository;
-    private SportRepository sportRepository;
 
-    public AthleteController(AthleteRepository athleteRepository,
-                             SportRepository sportRepository) {
+    public AthleteController(AthleteRepository athleteRepository) {
         this.athleteRepository = athleteRepository;
-        this.sportRepository = sportRepository;
     }
 
     @GetMapping("/athletes")
     public Iterable<Athlete> getAthletes() {
         return athleteRepository.findAll();
-    }
-
-    @GetMapping("/test")
-    public Athlete test() {
-        Athlete existingAthlete = athleteRepository.findByName("Petter Northug Jr.");
-
-        // update an existing result, and add a new one
-        List<Result> results = existingAthlete.getResults();
-        Result existingResult = results.get(0);
-        existingResult.setAge(100);
-        existingResult.setGold(200);
-
-        Sport cycling = sportRepository.findByName("Cycling");
-        Result newResult = new Result(cycling, 101, 2017, "01/01/2017", 1, 2, 3);
-        results.add(newResult);
-
-        return athleteRepository.save(existingAthlete);
     }
 
     @GetMapping("/athlete")
